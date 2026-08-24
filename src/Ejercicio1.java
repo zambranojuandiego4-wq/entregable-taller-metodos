8220f723c4bac0a8575f295e2d3cab6cff0e1095
juandiego2009 <zambranojuandiego4@gmail.com>
2026-08-24 10:48:21 -0500

    ejercicio1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+
+    public static void mostrarBienvenida() {
+        System.out.println("Bienvenido al Curso de Programación de java");
+    }
+
+    public static void main(String[] args) {
+        mostrarBienvenida();
+    }
+}
+
