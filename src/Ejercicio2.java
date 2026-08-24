965744a0f8700e03bb2655c59de0aa2ea547a5ea
juandiego2009 <zambranojuandiego4@gmail.com>
2026-08-24 10:52:43 -0500

    ejercicio2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Ejercicio2 {
+
+    public static void saludarEstudiante(String nombre, int edad) {
+        System.out.println("Hola " + nombre + ", tu edad registrada es: " + edad + " años.");
+    }
+
+
+    public static void main(String[] args) {
+        saludarEstudiante("Juan", 17);
+    }
+}
